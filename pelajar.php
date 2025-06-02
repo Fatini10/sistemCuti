6168096bf5a2b3a783b53787a4c967a0eaa8a2c9
unknown <jananiratha24@gmail.com>
2025-06-02 11:37:35 +0800

    masukkan copyRight

--- pelajar.php
+++ pelajar.php
@@ -125,5 +125,9 @@ include ('header.php');
         </div>
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
+<center>
+<footer>© 2025 Sistem Permohonan Cuti. Hak cipta terpelihara.</footer>
+</center>
 </html>
